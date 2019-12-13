226857c837b640e716697b26cf44ac2bd3bf0cc8
Farhan Ahmed <fah@instagram.com>
2019-12-12 22:09:32 -0500

    JFC that took long - Day 7 solution

--- day7/Program.fs
+++ day7/Program.fs
@@ -50,68 +50,131 @@ let getVal (prog: int[]) (pm: Parameter) =
     | 1 -> pm.param
     | _ -> raise (InnerError("incorrect parameter mode"))
 
-let rec execute prog pos (inputs: int list) (lastval: int) =
-    let inst = getInstruction prog pos
-    //printfn "%A" prog
-    //printfn "%A" inst
-    match inst.opcode with
-    | 99 -> lastval
-    | 1 -> 
-        let v1 = getVal prog inst.pms.[0]
-        let v2 = getVal prog inst.pms.[1]
-        let addr = inst.pms.[2].param
-        //printfn "%d %d %d" v1 v2 addr
-        Array.set prog addr (v1 + v2)
-        execute prog (pos + inst.offset) inputs lastval
-    | 2 ->
-        let v1 = getVal prog inst.pms.[0]
-        let v2 = getVal prog inst.pms.[1]
-        let addr = inst.pms.[2].param
-        Array.set prog addr (v1 * v2)
-        execute prog (pos + inst.offset) inputs lastval
-    | 3 -> 
-        //printfn "%A" inputs 
-        let inp = (List.head inputs)
-        let addr = inst.pms.[0].param
-        Array.set prog addr inp 
-        execute prog (pos + inst.offset) (List.tail inputs) lastval
-    | 4 ->
-        let v = getVal prog inst.pms.[0]
-        //printfn "%A" v
-        execute prog (pos + inst.offset) inputs v     
-    | 5 ->
-        let v = getVal prog inst.pms.[0]
-        match v with
-        | a when a <> 0 -> 
-            let ptr = getVal prog inst.pms.[1]
-            execute prog (ptr) inputs lastval
-        | a -> execute prog (pos + inst.offset) inputs lastval
-    | 6 ->
-        let v = getVal prog inst.pms.[0]
-        //printfn "%A" v
-        match v with
-        | a when a = 0 -> 
-            let ptr = getVal prog inst.pms.[1]
-            //printfn "%A" off
-            execute prog (ptr) inputs lastval
-        | a -> execute prog (pos + inst.offset) inputs lastval
-    | 7 ->
-        let v1 = getVal prog inst.pms.[0]
-        let v2 = getVal prog inst.pms.[1]
-        if v1 < v2 then 
-            Array.set prog inst.pms.[2].param 1
-        else
-            Array.set prog inst.pms.[2].param 0
-        execute prog (pos + inst.offset) inputs lastval
-    | 8 ->
-        let v1 = getVal prog inst.pms.[0]
-        let v2 = getVal prog inst.pms.[1]
-        if v1 = v2 then 
-            Array.set prog inst.pms.[2].param 1
-        else
-            Array.set prog inst.pms.[2].param 0
-        execute prog (pos + inst.offset) inputs lastval
-    | _ -> raise (InnerError("Wrong opcode")) 
+type Payload = int * AsyncReplyChannel<string>
+type Message = 
+    | Combined of Payload * (MailboxProcessor<Message> option)
+
+type Agent = MailboxProcessor<Message>
+
+type Count = int * AsyncReplyChannel<string>
+
+let referee = MailboxProcessor<Count>.Start(fun inbox ->
+    let mutable highest = 0
+    let rec loop () =
+        async {
+            let! (i, rc) = inbox.Receive()
+            if i > highest then 
+                highest <- i
+                //printfn "New highest: %A" highest
+            elif i = 777 then
+                printfn "Highest: %A" highest 
+            rc.Reply(String.Format("Received {0}", i))
+            return! loop ()
+        }
+    loop ()
+)
+
+let agent prog pos num = MailboxProcessor<Message>.Start(fun inbox ->
+    //printfn "Starting agent %d ...\n" num
+    let rec execute prog pos (input: int) (lastval: int) (output) =
+        async {
+            let mutable npos = pos
+            let mutable ninp = input
+            let mutable lval = lastval
+            let mutable nout = output
+
+            let inst = getInstruction prog pos
+            
+            //if num = 5 then 
+            //    printfn "Num: %d\n%A" num inst
+            
+            match inst.opcode with
+            | 99 -> //printfn "Num: %d halted. Last val =%A" num lval
+                    if num = 5 then 
+                        //printfn "%A" lval
+                        referee.PostAndReply(fun rc -> lval, rc) |> ignore
+                    raise (InnerError("Thread done."))
+            | 1 -> 
+                let v1 = getVal prog inst.pms.[0]
+                let v2 = getVal prog inst.pms.[1]
+                let addr = inst.pms.[2].param
+                Array.set prog addr (v1 + v2)
+                npos <- pos + inst.offset
+            | 2 ->
+                let v1 = getVal prog inst.pms.[0]
+                let v2 = getVal prog inst.pms.[1]
+                let addr = inst.pms.[2].param
+                Array.set prog addr (v1 * v2)
+                npos <- pos + inst.offset
+            | 3 ->      
+                //printfn "Waiting for input ..."    
+                let! inp = inbox.Receive()
+                //printfn "%d Message received: %A" num inp
+                match inp with
+                | Combined (payload, out) -> 
+                    let (i, rc) = payload
+                    ninp <- i
+                    match out with
+                    | Some x -> nout <- x :: nout
+                    | None -> ()
+                    rc.Reply(String.Format("{0} message received", num))
+
+                let addr = inst.pms.[0].param
+                Array.set prog addr ninp
+
+                npos <- pos + inst.offset
+            | 4 ->
+                lval <- getVal prog inst.pms.[0]
+
+                //printfn "Output= %A" lval
+                //printfn "%A" nout
+
+                let ochan = List.head nout
+                let reply = ochan.TryPostAndReply((fun rc -> Combined (Payload (lval, rc), None)), 100)
+                //printfn "reply= %A" reply
+
+                npos <- pos + inst.offset               
+            | 5 ->
+                let v = getVal prog inst.pms.[0]
+                match v with
+                | a when a <> 0 -> 
+                    let ptr = getVal prog inst.pms.[1]
+                    npos <- ptr
+                | a -> npos <- pos + inst.offset
+            | 6 ->
+                let v = getVal prog inst.pms.[0]
+                //printfn "%A" v
+                match v with
+                | a when a = 0 -> 
+                    let ptr = getVal prog inst.pms.[1]
+                    //printfn "%A" off
+                    npos <- ptr
+                | a -> npos <- pos + inst.offset
+            | 7 ->
+                let v1 = getVal prog inst.pms.[0]
+                let v2 = getVal prog inst.pms.[1]
+                if v1 < v2 then 
+                    Array.set prog inst.pms.[2].param 1
+                else
+                    Array.set prog inst.pms.[2].param 0
+                npos <- pos + inst.offset                   
+            | 8 ->
+                let v1 = getVal prog inst.pms.[0]
+                let v2 = getVal prog inst.pms.[1]
+                if v1 = v2 then 
+                    Array.set prog inst.pms.[2].param 1
+                else
+                    Array.set prog inst.pms.[2].param 0
+                npos <- pos + inst.offset    
+            | _ -> raise (InnerError("Wrong opcode")) 
+
+            //printfn "%A %A %A %A" npos ninp lval nout
+            return! execute prog npos ninp lval nout
+        }
+
+    //printfn "Here"
+    execute prog pos 0 0 []
+)
 
 (*
 let part1 = 
@@ -133,8 +196,14 @@ let part1 =
     signals 
 *)
 
+let makeAgent id = async {
+    agent program 0 id |> ignore
+}
 
-let part2 = 
+[<EntryPoint>]
+let main argv =
+    //let p = part1                        
+    //printfn "%A" part1
     let mutable signals = 0
     for a in [5 .. 9] do
         for b in [5 .. 9] do 
@@ -143,21 +212,40 @@ let part2 =
                     for e in [5 .. 9] do
                         let phases = [a;b;c;d;e]
                         if (Set.ofList phases).Count = 5 then
-                            let r1 = execute program 0 [a; 0] 0
-                            let r2 = execute program 0 [b; r1] 0
-                            let r3 = execute program 0 [c; r2] 0
-                            let r4 = execute program 0 [d; r3] 0
-                            let r5 = execute program 0 [e; r4] 0
-                            if r5 > signals then signals <- r5 else ()
+                            
+                            printfn "%A" phases
+
+                            let v1 = agent program 0 1
+                            let v2 = agent program 0 2
+                            let v3 = agent program 0 3
+                            let v4 = agent program 0 4
+                            let v5 = agent program 0 5
+
+                            let reply = v1.PostAndReply(fun rc -> Combined (Payload (a, rc), Some v2))
+                            //printfn "reply= %A" reply
+
+                            let reply = v2.PostAndReply(fun rc -> Combined (Payload (b, rc), Some v3))
+                            //printfn "reply= %A" reply
+
+                            let reply = v3.PostAndReply(fun rc -> Combined (Payload (c, rc), Some v4))
+                            //printfn "reply= %A" reply
+
+                            let reply = v4.PostAndReply(fun rc -> Combined (Payload (d, rc), Some v5))
+                            //printfn "reply= %A" reply
+
+                            let reply = v5.PostAndReply(fun rc -> Combined (Payload (e, rc), Some v1))
+                            //printfn "reply= %A" reply
+
+                            let reply = v1.PostAndReply(fun rc -> Combined (Payload (0, rc), None))
+                            //printfn "reply= %A" reply
+                            
+                            //printfn "Waiting"
+                            //let x = Console.ReadLine()
+                            System.Threading.Thread.Sleep(1000)
+                            ()    
                         else ()
-    signals                
 
+    referee.PostAndReply(fun rc -> 777, rc) |> ignore
+    let x = Console.ReadLine()
 
-[<EntryPoint>]
-let main argv =
-    //let p = part1                        
-    //printfn "%A" part1
-
-    let v = part2
-    printfn "%A" v
     0 // return an integer exit code
